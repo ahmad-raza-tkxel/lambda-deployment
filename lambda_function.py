fea8d5262ccb52f693ef95859220e7997f43eef8
Ahmad Raza <ahmad.raza@camp1.tkxel.com>
2023-05-31 18:06:36 +0500

    git-action changed

--- lambda_function.py
+++ lambda_function.py
@@ -1,9 +1,10 @@
 import json
+import requests
 
 
 def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Change by github action!')
+        'body': json.dumps('Next by github action!')
     }
